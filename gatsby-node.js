8fed75a021633096cd813f6c839095ba78bea6b0
Kyle Gill <kylerobertgill@gmail.com>
2019-11-19 14:32:17 -0700

    use example from docs

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,15 @@
  */
 
 // You can delete this file if you're not using it
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage, deletePage } = actions
+  deletePage(page)
+  // You can access the variable "house" in your page queries now
+  createPage({
+    ...page,
+    context: {
+      ...page.context,
+      house: `Gryffindor`,
+    },
+  })
+}
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,13 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = ({ pageContext }) =>
+  console.log(pageContext) || (
+    <Layout>
+      <SEO title="Page two" />
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2, open the console</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
 
 export default SecondPage
